2fc2c7a8283f3f5176b834ef1489ec69ce8cd55d
tuanVGU <cs2015_tuan.ld@student.vgu.edu.vn>
2018-03-10 22:02:28 +0700

    Ex4 Updated

--- translator/src/document/testClass.java
+++ translator/src/document/testClass.java
@@ -5,5 +5,14 @@ public class testClass {
         TextDocument t1 = new TextDocument("abc",TextDocument.Encoding.UTF8);
         TextDocument t2 = new TextDocument("abc",TextDocument.Encoding.UTF16);
         TextDocument t3 = new TextDocument("abc",TextDocument.Encoding.UTF32);
+        complexDocument doc0 = new complexDocument();
+        TextDocument doc2 = new TextDocument("The final exam take place soon",TextDocument.Encoding.UTF8);
+        doc0.addComponent(doc2);
+        complexDocument doc3 = new complexDocument();
+        TextDocument doc5 = new TextDocument("Software Engineering Design is a lecture at the VGU",TextDocument.Encoding.UTF32);
+        GraficDocument doc4 = new GraficDocument("localhost:8080");
+        doc3.addComponent(doc4);
+        doc3.addComponent(doc5);
+        doc0.addComponent(doc3);
     }
 }
